f058279bda02e3d97b76a3560457ddd2d2323325
LarryMod <larrynicoletta1@gmail.com>
2026-08-25 17:57:15 -0400

    Add 3 more Kevin MacLeod music in the sound library

--- src/lib/libraries/other-sound-previews/sound-previews.js
+++ src/lib/libraries/other-sound-previews/sound-previews.js
@@ -3,11 +3,17 @@ import sonarPing from './sonar-ping.png';
 import river2 from './river-2.png';
 import elevatorDing from './elevator-ding.png';
 import nanoHoedown from './nano-hoedown.png';
+import monkeysSpinningMonkeys from './monkeys-spinning-monkeys.png';
+import fluffingADuck from './fluffing-a-duck.png';
+import hallOfTheMountainKing from './hall-of-the-mountain-king.png';
 
 export default {
     "mibup15full": mibup15full,
     "sonar-ping": sonarPing,
     "river-2": river2,
     "elevator-ding": elevatorDing,
-    "nano-hoedown": nanoHoedown
+    "nano-hoedown": nanoHoedown,
+    "monkeys-spinning-monkeys": monkeysSpinningMonkeys,
+    "fluffing-a-duck": fluffingADuck,
+    "hall-of-the-mountain-king": hallOfTheMountainKing,
 }
