4b82208fccca900f4fdc9524dcfcdb0e0afcaf98
LarryMod <larrynicoletta1@gmail.com>
2026-08-27 16:35:26 -0400

    Update nb-encode-mp3-worker.js

--- src/lib/nb-encode-mp3-worker.js
+++ src/lib/nb-encode-mp3-worker.js
@@ -15,26 +15,24 @@ self.onmessage = event => {
 
     // Channels must be converted from Float32Arrays to Int16Arrays to prevent registering as near-silence.
     // The encoder expects values between -32768 and 32767, and our arrays have values between -1.0 and 1.0.
-    for (let i = 0; i < channel1Samples.length; i++) {
-        const sample1 = Math.max(-1, Math.min(channel1Samples[i], 1));
-        left[i] = sample1 < 0 ? sample1 * 0x8000 : sample1 * 0x7FFF;
-
-        if (channel2Samples) {
-            const sample2 = Math.max(-1, Math.min((channel2Samples)[i], 1));
-            right[i] = sample2 < 0 ? sample2 * 0x8000 : sample2 * 0x7FFF;
-        }
-    }
-
-    const sampleBlockSize = 1152;
-
     for (let i = 0; i < left.length; i += sampleBlockSize) {
-        const leftChunk = left.subarray(i, i + sampleBlockSize);
+    const leftChunk = left.subarray(i, i + sampleBlockSize);
+
+    if (channel2Samples) {
         const rightChunk = right.subarray(i, i + sampleBlockSize);
         const buffer = encoder.encodeBuffer(leftChunk, rightChunk);
+
+        if (buffer.length > 0) {
+            chunks.push(buffer);
+        }
+    } else {
+        const buffer = encoder.encodeBuffer(leftChunk);
+
         if (buffer.length > 0) {
             chunks.push(buffer);
         }
     }
+}
 
     const flushed = encoder.flush();
     if (flushed.length > 0) {
