3d29a67c6c9d7c0d39ce21b2b2bf0176f2629a39
LarryMod <larrynicoletta1@gmail.com>
2026-08-28 21:26:01 -0400

    Make ice cube color menu functional

--- src/components/menu-bar/color-of-the-ice-cube-menu.jsx
+++ src/components/menu-bar/color-of-the-ice-cube-menu.jsx
@@ -4,34 +4,60 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 import {connect} from 'react-redux';
-import locales from '@turbowarp/scratch-l10n';
 
 import check from './check.svg';
 import {MenuItem, Submenu} from '../menu/menu.jsx';
 import colorOfTheIceCubeIcon from '../color-of-the-ice-cube-selector/color-of-the-ice-cube-icon.svg';
 import {
     colorOfTheIceCubeMenuOpen,
-    openColorOfTheIceCubeMenu
+    openColorOfTheIceCubeMenu,
+    closeColorOfTheIceCubeMenu
 } from '../../reducers/menus.js';
-import {selectLocale} from '../../reducers/locales.js';
+import {iceCubeColor, setIceCubeColor} from '../../reducers/ice-cube-color.js';
+import iceCubeColors from '../../lib/ice-cube-colors.js';
 
 import styles from './settings-menu.css';
 
 import dropdownCaret from './dropdown-caret.svg';
+
+const ICE_CUBE_COLOR_STORAGE_KEY = 'pm:iceCubeColor';
+
+const applyIceCubeColor = color => {
+    if (typeof document === 'undefined') return;
+    document.documentElement.style.setProperty('--ice-cube-color', color);
+    if (typeof window !== 'undefined') {
+        try {
+            window.localStorage.setItem(ICE_CUBE_COLOR_STORAGE_KEY, color);
+        } catch (e) {
+            // Ignore storage failures such as private browsing restrictions.
+        }
+        window.dispatchEvent(new CustomEvent('IceCubeColorChange', {
+            detail: {color: color}
+        }));
+    }
+};
 
 class ColorOfTheIceCubeMenu extends React.PureComponent {
     constructor (props) {
         super(props);
         bindAll(this, [
             'setRef',
-            'handleMouseOver'
+            'handleMouseOver',
+            'handleChangeColor'
         ]);
+    }
+
+    componentDidMount () {
+        applyIceCubeColor(this.props.currentColor);
     }
 
     componentDidUpdate (prevProps) {
         // If the submenu has been toggled open, try scrolling the selected option into view.
         if (!prevProps.menuOpen && this.props.menuOpen && this.selectedRef) {
             this.scrollSelectedIntoView();
+        }
+        if (prevProps.currentColor !== this.props.currentColor) {
+            applyIceCubeColor(this.props.currentColor);
         }
     }
 
@@ -46,11 +72,13 @@ class ColorOfTheIceCubeMenu extends React.PureComponent {
         }
     }
 
+    handleChangeColor (color) {
+        this.props.onChangeColorOfTheIceCube(color);
+    }
+
     scrollSelectedIntoView () {
         // the native scrollIntoView() scrolls the entire page when used outside the editor,
         // so we do this manually instead.
-        // selectedRef is the checkmark <img>, its parent is a <div> from <MenuItem>, then a <div> from <SubMenu>
-
         const menuItem = this.selectedRef.parentNode;
         const scrollContainer = menuItem.parentNode;
 
@@ -94,25 +122,28 @@ class ColorOfTheIceCubeMenu extends React.PureComponent {
                     place={this.props.isRtl ? 'left' : 'right'}
                 >
                     {
-                        Object.keys(locales)
-                            .map(locale => (
-                                <MenuItem
-                                    key={locale}
-                                    className={styles.colorOfTheIceCubeMenuItem}
-                                    // eslint-disable-next-line react/jsx-no-bind
-                                    onClick={() => this.props.onChangeColorOfTheIceCube(locale)}
-                                >
-                                    <img
-                                        className={classNames(styles.check, {
-                                            [styles.selected]: this.props.currentLocale === locale
-                                        })}
-                                        src={check}
-                                        draggable={false}
-                                        {...(this.props.currentLocale === locale && {ref: this.setRef})}
-                                    />
-                                    {locales[locale].name}
-                                </MenuItem>
-                            ))
+                        iceCubeColors.map(color => (
+                            <MenuItem
+                                key={color.value}
+                                className={styles.colorOfTheIceCubeMenuItem}
+                                // eslint-disable-next-line react/jsx-no-bind
+                                onClick={() => this.handleChangeColor(color.value)}
+                            >
+                                <span
+                                    className={styles.iceCubeColorSwatch}
+                                    style={{backgroundColor: color.value}}
+                                />
+                                <img
+                                    className={classNames(styles.check, {
+                                        [styles.selected]: this.props.currentColor === color.value
+                                    })}
+                                    src={check}
+                                    draggable={false}
+                                    {...(this.props.currentColor === color.value && {ref: this.setRef})}
+                                />
+                                {color.name}
+                            </MenuItem>
+                        ))
                     }
                 </Submenu>
             </MenuItem>
@@ -121,25 +152,25 @@ class ColorOfTheIceCubeMenu extends React.PureComponent {
 }
 
 ColorOfTheIceCubeMenu.propTypes = {
-    currentLocale: PropTypes.string,
+    currentColor: PropTypes.string,
     isRtl: PropTypes.bool,
-    label: PropTypes.string,
     menuOpen: PropTypes.bool,
-    onChangeColorOfTheIceCube: PropTypes.func,
-    onRequestCloseSettings: PropTypes.func,
-    onRequestOpen: PropTypes.func
+    onChangeColorOfTheIceCube: PropTypes.func.isRequired,
+    onRequestCloseSettings: PropTypes.func.isRequired,
+    onRequestOpen: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-    currentLocale: state.locales.locale,
+    currentColor: iceCubeColor(state),
     isRtl: state.locales.isRtl,
-    menuOpen: colorOfTheIceCubeMenuOpen(state),
-    messagesByLocale: state.locales.messagesByLocale
+    menuOpen: colorOfTheIceCubeMenuOpen(state)
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    onChangeColorOfTheIceCube: locale => {
-        dispatch(selectLocale(locale));
+    onChangeColorOfTheIceCube: color => {
+        dispatch(setIceCubeColor(color));
+        applyIceCubeColor(color);
+        dispatch(closeColorOfTheIceCubeMenu());
         ownProps.onRequestCloseSettings();
     },
     onRequestOpen: () => dispatch(openColorOfTheIceCubeMenu())
