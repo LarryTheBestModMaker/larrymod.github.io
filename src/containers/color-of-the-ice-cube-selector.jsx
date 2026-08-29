96f2b11d7923b399654fc8efb8d5914128749e84
LarryMod <larrynicoletta1@gmail.com>
2026-08-28 21:26:18 -0400

    Make ice cube color selector functional

--- src/containers/color-of-the-ice-cube-selector.jsx
+++ src/containers/color-of-the-ice-cube-selector.jsx
@@ -2,10 +2,28 @@ import bindAll from 'lodash.bindall';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {connect} from 'react-redux';
-import {selectLocale} from '../reducers/locales';
+
 import {closeColorOfTheIceCubeMenu} from '../reducers/menus';
+import {iceCubeColor, setIceCubeColor} from '../reducers/ice-cube-color';
 
 import ColorOfTheIceCubeSelectorComponent from '../components/color-of-the-ice-cube-selector/color-of-the-ice-cube-selector.jsx';
+
+const ICE_CUBE_COLOR_STORAGE_KEY = 'pm:iceCubeColor';
+
+const applyIceCubeColor = color => {
+    if (typeof document === 'undefined') return;
+    document.documentElement.style.setProperty('--ice-cube-color', color);
+    if (typeof window !== 'undefined') {
+        try {
+            window.localStorage.setItem(ICE_CUBE_COLOR_STORAGE_KEY, color);
+        } catch (e) {
+            // Ignore storage failures such as private browsing restrictions.
+        }
+        window.dispatchEvent(new CustomEvent('IceCubeColorChange', {
+            detail: {color: color}
+        }));
+    }
+};
 
 class ColorOfTheIceCubeSelector extends React.Component {
     constructor (props) {
@@ -13,24 +31,32 @@ class ColorOfTheIceCubeSelector extends React.Component {
         bindAll(this, [
             'handleChange'
         ]);
-        document.documentElement.lang = props.currentLocale;
     }
-    handleChange (e) {
-        const newLocale = e.target.value;
-        if (this.props.messagesByLocale[newLocale]) {
-            this.props.onChangeColorOfTheIceCube(newLocale);
-            document.documentElement.lang = newLocale;
+
+    componentDidMount () {
+        applyIceCubeColor(this.props.currentColor);
+    }
+
+    componentDidUpdate (prevProps) {
+        if (prevProps.currentColor !== this.props.currentColor) {
+            applyIceCubeColor(this.props.currentColor);
         }
     }
+
+    handleChange (e) {
+        this.props.onChangeColorOfTheIceCube(e.target.value);
+    }
+
     render () {
         const {
+            currentColor,
             onChangeColorOfTheIceCube, // eslint-disable-line no-unused-vars
-            messagesByLocale, // eslint-disable-line no-unused-vars
             children,
             ...props
         } = this.props;
         return (
             <ColorOfTheIceCubeSelectorComponent
+                currentColor={currentColor}
                 onChange={this.handleChange}
                 {...props}
             >
@@ -42,20 +68,18 @@ class ColorOfTheIceCubeSelector extends React.Component {
 
 ColorOfTheIceCubeSelector.propTypes = {
     children: PropTypes.node,
-    currentLocale: PropTypes.string.isRequired,
-    // Only checking key presence for messagesByLocale, no need to be more specific than object
-    messagesByLocale: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+    currentColor: PropTypes.string.isRequired,
     onChangeColorOfTheIceCube: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-    currentLocale: state.locales.locale,
-    messagesByLocale: state.locales.messagesByLocale
+    currentColor: iceCubeColor(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-    onChangeColorOfTheIceCube: locale => {
-        dispatch(selectLocale(locale));
+    onChangeColorOfTheIceCube: color => {
+        dispatch(setIceCubeColor(color));
+        applyIceCubeColor(color);
         dispatch(closeColorOfTheIceCubeMenu());
     }
 });
