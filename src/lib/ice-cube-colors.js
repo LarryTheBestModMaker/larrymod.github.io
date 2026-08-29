075ab2231a01b8b5ffec82a8d7e78b112110c88f
LarryMod <larrynicoletta1@gmail.com>
2026-08-28 21:25:13 -0400

    Add ice cube color options

---
+++ src/lib/ice-cube-colors.js
@@ -0,0 +1,20 @@
+const ICE_CUBE_COLORS = [
+    {name: 'Ice Blue', value: '#8ed8ff'},
+    {name: 'Blue', value: '#0084ff'},
+    {name: 'Cyan', value: '#00d9ff'},
+    {name: 'Teal', value: '#00c8a0'},
+    {name: 'Green', value: '#62d84e'},
+    {name: 'Lime', value: '#a8ff00'},
+    {name: 'Yellow', value: '#ffd83d'},
+    {name: 'Orange', value: '#ff9a3d'},
+    {name: 'Red', value: '#ff4d4d'},
+    {name: 'Pink', value: '#ff74c8'},
+    {name: 'Purple', value: '#a86bff'},
+    {name: 'Violet', value: '#c36bff'},
+    {name: 'Indigo', value: '#6c7cff'},
+    {name: 'White', value: '#ffffff'},
+    {name: 'Gray', value: '#b7c0cc'},
+    {name: 'Black', value: '#30343b'}
+];
+
+export default ICE_CUBE_COLORS;
