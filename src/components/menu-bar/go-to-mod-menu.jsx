fa6837edacb3745eafd2730e5dd990557a691019
LarryMod <larrynicoletta1@gmail.com>
2026-08-29 06:45:14 -0400

    Update go-to-mod-menu.jsx

--- src/components/menu-bar/go-to-mod-menu.jsx
+++ src/components/menu-bar/go-to-mod-menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {FormattedMessage} from 'react-intl';
 
 import {MenuItem, Submenu} from '../menu/menu.jsx';
 
