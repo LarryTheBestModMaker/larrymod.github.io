1bb4486895dcad90af49a3eee65383e0586837bd
LarryMod <larrynicoletta1@gmail.com>
2026-08-28 21:26:10 -0400

    Fix ice cube color selector

--- src/components/color-of-the-ice-cube-selector/color-of-the-ice-cube-selector.jsx
+++ src/components/color-of-the-ice-cube-selector/color-of-the-ice-cube-selector.jsx
@@ -1,36 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import locales from '@turbowarp/scratch-l10n';
+import iceCubeColors from '../../lib/ice-cube-colors.js';
 import styles from './color-of-the-ice-cube-selector.css';
 
-// supported languages to exclude from the menu, but allow as a URL option
-const ignore = [];
-
-const ColorOfTheIceCubeSelector = ({currentLocale, label, onChange}) => (
+const ColorOfTheIceCubeSelector = ({currentColor, label, onChange}) => (
     <select
         aria-label={label}
         className={styles.colorOfTheIceCubeSelect}
-        value={currentLocale}
+        value={currentColor}
         onChange={onChange}
     >
         {
-            Object.keys(locales)
-                .filter(l => !ignore.includes(l))
-                .map(locale => (
-                    <option
-                        key={locale}
-                        value={locale}
-                    >
-                        {locales[locale].name}
-                    </option>
-                ))
+            iceCubeColors.map(color => (
+                <option
+                    key={color.value}
+                    value={color.value}
+                >
+                    {color.name}
+                </option>
+            ))
         }
     </select>
 );
 
 ColorOfTheIceCubeSelector.propTypes = {
-    currentLocale: PropTypes.string,
+    currentColor: PropTypes.string,
     label: PropTypes.string,
     onChange: PropTypes.func
 };
