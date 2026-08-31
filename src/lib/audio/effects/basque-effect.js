64244d69b74d514ae39c40ff0edb3efce3be21b2
LarryMod <larrynicoletta1@gmail.com>
2026-08-31 14:46:55 -0400

    Give back Audio Translator

---
+++ src/lib/audio/effects/basque-effect.js
@@ -0,0 +1,52 @@
+class BasqueEffect {
+    constructor(audioContext, startSeconds, endSeconds, targetLanguage = "Basque") {
+        this.audioContext = audioContext;
+        this.input = this.audioContext.createGain();
+        this.output = this.audioContext.createGain();
+
+        this.targetLanguage = targetLanguage;
+
+        this.translatedSpeech = this.audioContext.createGain();
+        this.translatedSpeech.gain.value = 1;
+
+        this.input.connect(this.translatedSpeech);
+        this.translatedSpeech.connect(this.output);
+
+        this.startSeconds = startSeconds;
+        this.endSeconds = endSeconds;
+
+        this.translationResult = null;
+    }
+
+    async translate(audioBlob) {
+        const text = await speechToText(audioBlob);
+
+        const translatedText = await translateText(
+            text,
+            this.targetLanguage
+        );
+
+        const translatedAudio = await textToSpeech(
+            translatedText,
+            this.targetLanguage
+        );
+
+        const audioBuffer = await this.audioContext.decodeAudioData(
+            await translatedAudio.arrayBuffer()
+        );
+
+        const source = this.audioContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(this.translatedSpeech);
+
+        source.start(this.startSeconds);
+
+        this.translationResult = {
+            originalText: text,
+            translatedText: translatedText,
+            language: this.targetLanguage
+        };
+    }
+}
+
+export default BasqueEffect;
